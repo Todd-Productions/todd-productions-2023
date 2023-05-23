2c0f50f95a3258b12a0ac08b290b8ffaaee57958
webpandadesigns <andrew@toddprod.com>
2023-05-23 12:51:50 -0400

    Installing basic packages

---
+++ config/colors.js
@@ -0,0 +1,34 @@
+module.exports = {
+  white: "#fff",
+  black: "#000",
+
+  // Green
+  green300: "#5a9d7d",
+  green500: "#026937",
+  green800: "#002c06",
+
+  // Blue
+  blue500: "#008ebe",
+  blue700: "#1f6366",
+
+  // Orange
+  orange500: "#f7901d",
+
+  // Yellow
+  yello500: "#f5a700",
+
+  // Tan
+  tan500: "#9d958d",
+
+  // Grey
+  grey200: "#f5f5f5",
+  grey300: "#ccc",
+  grey400: "#808080",
+  grey700: "#4d4d4d",
+
+  // Red
+  red500: "#c91429",
+
+  facebook: "#3c5b9c",
+  youtube: "#c92b29",
+};
