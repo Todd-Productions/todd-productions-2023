002f214194720deacb89cdb512ca6c9b3ab2478a
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2023-06-15 14:15:51 -0700

    Updated SplitContentSection, added to HomeTemplate

--- src/ui/molecules/SplitContentSection/SplitContentSection.tsx
+++ src/ui/molecules/SplitContentSection/SplitContentSection.tsx
@@ -4,7 +4,7 @@ import Link from "next/link"
 
 import { Button, SectionHeading, SectionSubHeading } from "../../atoms"
 
-interface SplitContentSectionProps {
+export interface ISplitContent {
   imageSrc: string
   imageAlt: string
   title: string
@@ -12,10 +12,12 @@ interface SplitContentSectionProps {
   textContent: string
   buttonLink: string
   buttonText: string
-  imgRight?: boolean
+  imgleft?: boolean
 }
 
-const SplitContentSection: React.FC<SplitContentSectionProps> = ({
+export interface SplitContentProps extends ISplitContent {}
+
+const SplitContentSection: React.FC<SplitContentProps> = ({
   imageSrc,
   imageAlt,
   title,
@@ -23,24 +25,27 @@ const SplitContentSection: React.FC<SplitContentSectionProps> = ({
   textContent,
   buttonLink,
   buttonText,
-  imgRight,
+  imgleft,
 }) => {
+  const gridColSizes = imgleft
+    ? "md:grid-cols-[2fr_1fr]"
+    : "md:grid-cols-[1fr_2fr]"
+  const order = imgleft ? "md:order-1" : "md:order-2"
+  const order2 = imgleft ? "md:order-2" : "md:order-1"
+
   return (
     <section className="bg-white">
-      <div
-        className={`grid grid-cols-[${
-          imgRight ? "2fr_1fr" : "1fr_2fr"
-        }] h-full`}
-      >
-        <div className={`relative ${imgRight ? "order-1" : "order-2"}`}>
-          <Image src={imageSrc} alt={imageAlt} fill />
+      <div className={`grid ${gridColSizes} h-full`}>
+        <div className={`h-60 md:h-auto relative order-1 ${order}`}>
+          <Image className="object-cover" src={imageSrc} alt={imageAlt} fill />
         </div>
-        <div className={`p-16  ${imgRight ? "order-2" : "order-1"}`}>
+
+        <div className={`p-10 md:p-16 order-2  ${order2}`}>
           <SectionHeading>{title}</SectionHeading>
           <SectionSubHeading>{subTitle}</SectionSubHeading>
           <div className="mb-12 leading-9">{textContent}</div>
           <Link href={buttonLink}>
-            <Button>{buttonText}</Button>
+            <Button fullWidth>{buttonText}</Button>
           </Link>
         </div>
       </div>
--- src/ui/templates/HomeTemplate/HomeTemplate.tsx
+++ src/ui/templates/HomeTemplate/HomeTemplate.tsx
@@ -5,11 +5,18 @@ import { getSocialLinks } from "../../molecules/Header/Header"
 
 import { INavLink, ISocialLinks } from "../../../types"
 import { ICrumb } from "../../molecules/Breadcrumbs/Breadcrumbs"
+import { SplitContentSection } from "../../molecules"
+
+// @types
+import { ISplitContent } from "../../molecules/SplitContentSection/SplitContentSection"
 
 export interface DefaultTemplateProps extends ISocialLinks {
   crumbs: ICrumb[]
   headerLinks: INavLink[]
   footerLinks: INavLink[]
+  videoData: ISplitContent
+  webData: ISplitContent
+  otherData: ISplitContent
 }
 
 export interface HomeTemplateProps extends DefaultTemplateProps {}
@@ -24,7 +31,37 @@ const HomeTemplate: React.FC<HomeTemplateProps> = (props) => {
       footerLinks={footerLinks}
       {...getSocialLinks(props)}
     >
-      Home Template
+      <SplitContentSection
+        title="video production"
+        subTitle="SALES, COMMERICAL, MUSIC, AND MORE"
+        textContent="Videos are an excellent way to convey just the right message to your audience, and we make it easy! From script development to filming to audio recording and post-production editing, we’ll work closely with your staff to ensure that the end product exceeds your goals."
+        buttonLink="/"
+        buttonText="see video types"
+        imageSrc="https://source.unsplash.com/random"
+        imageAlt=""
+        imgleft
+      />
+
+      <SplitContentSection
+        title="web services"
+        subTitle="CONQUER THE WEB"
+        textContent="Websites are the digital home of a business! While social media sites can serve as great marketing for you, businesses and organizations need to capitalize on both. At Todd Productions Inc. we’ll work with you to make your website the best that it can be."
+        buttonLink="/"
+        buttonText="view web services"
+        imageSrc="https://source.unsplash.com/random"
+        imageAlt=""
+      />
+
+      <SplitContentSection
+        title="other services"
+        subTitle="LOGO DESIGN, PRINT, AND MORE"
+        textContent="In this high-tech world of smart phones and tablets, sometimes the best marketing tool is the good old-fashioned printed piece. For many companies, brochures, flyers, and direct mail pieces are just one aspect of their integrated marketing strategy that cuts across old and new technology. Need a brochure, flyer or direct mail piece? Perhaps a new logo or new photography to use in marketing tools? A place to record voice over? Or help with your social media presence? Todd Productions Inc. can help!"
+        buttonLink="/"
+        buttonText="see other services"
+        imageSrc="https://source.unsplash.com/random"
+        imageAlt=""
+        imgleft
+      />
     </MainLayout>
   )
 }
