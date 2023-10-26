f346005f78e1934511a5a5b595f25f33d122ca1f
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2023-10-26 08:40:25 -0700

    Fixed yup import build issue

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,44 +1,41 @@
-// import { NextResponse } from "next/server"
-// import yup from "yup"
-// import sendEmail from "./email"
+import { NextResponse } from "next/server"
+import * as yup from "yup"
 
-// const schema = yup.object({
-//   name: yup.string().required(),
-//   email: yup.string().email().required(),
-//   message: yup.string().required(),
-// })
+import sendEmail from "./email"
 
-// const { EMAIL_TO } = process.env
+const schema = yup.object({
+  name: yup.string().required(),
+  email: yup.string().email().required(),
+  message: yup.string().required(),
+})
 
-// export const POST = async (request: Request) => {
-//   const res = await request.json()
-//   try {
-//     const { email, name, message } = await schema.validate(res)
-//     await sendEmail(
-//       EMAIL_TO ?? "contact@toddprod.com",
-//       "Contact Submission - Todd Productions Inc.",
-//       {
-//         email,
-//         name,
-//         message,
-//       }
-//     )
+const { EMAIL_TO } = process.env
 
-//     return NextResponse.json({
-//       message: "Successfully sent message",
-//     })
-//   } catch (error) {
-//     return NextResponse.json(
-//       {
-//         error,
-//       },
-//       {
-//         status: 400,
-//       }
-//     )
-//   }
-// }
+export const POST = async (request: Request) => {
+  const res = await request.json()
+  try {
+    const { email, name, message } = await schema.validate(res)
+    await sendEmail(
+      EMAIL_TO ?? "contact@toddprod.com",
+      "Contact Submission - Todd Productions Inc.",
+      {
+        email,
+        name,
+        message,
+      }
+    )
 
-// Build error - Failed to collect page data for /api/contact
-// Investigate when implementing
-export {}
+    return NextResponse.json({
+      message: "Successfully sent message",
+    })
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error,
+      },
+      {
+        status: 400,
+      }
+    )
+  }
+}
