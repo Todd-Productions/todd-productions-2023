985c790c7054fbbd78a6c1024017cd4abd54a17a
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2023-10-27 16:03:56 -0700

    Updated contact api code

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server"
 import * as yup from "yup"
+import fs from "fs"
+import path from "path"
 
 import sendEmail from "./email"
 
@@ -13,17 +15,22 @@ const { EMAIL_TO } = process.env
 
 export const POST = async (request: Request) => {
   const res = await request.json()
+  const templatePath = path.join(process.cwd(), `templates/contact.hbs`)
+
+  const emailTemplate = fs.readFileSync(templatePath, "utf8")
+
   try {
     const { email, name, message } = await schema.validate(res)
-    await sendEmail(
-      EMAIL_TO ?? "contact@toddprod.com",
-      "Contact Submission - Todd Productions Inc.",
-      {
+    await sendEmail({
+      to: EMAIL_TO ?? "contact@toddprod.com",
+      subject: "Contact Submission - Todd Productions Inc.",
+      context: {
         email,
         name,
         message,
-      }
-    )
+      },
+      template: emailTemplate,
+    })
 
     return NextResponse.json({
       message: "Successfully sent message",
