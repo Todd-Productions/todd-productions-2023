7f255c063556a73931039cc3bd66dc04099f633a
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2023-12-28 09:18:52 -0800

    Testing with bundle-analyzer

--- next.config.js
+++ next.config.js
@@ -5,9 +5,8 @@ const nextConfig = {
   },
 }
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 })
 
 module.exports = withBundleAnalyzer(nextConfig)
-
