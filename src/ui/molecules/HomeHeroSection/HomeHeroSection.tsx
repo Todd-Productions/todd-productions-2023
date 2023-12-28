e79f53ebb25490cc65f925ed607cab69dd5a81ce
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2023-12-28 10:08:05 -0800

    Updated home hero content styles and html structure

--- src/ui/molecules/HomeHeroSection/HomeHeroSection.tsx
+++ src/ui/molecules/HomeHeroSection/HomeHeroSection.tsx
@@ -26,41 +26,46 @@ const HomeHeroSection: React.FC<SplitContentProps> = ({
   button,
   bannerImg,
 }) => (
-  <section>
-    <Hero img={bannerImg} height={500} isVideo priority />
-    <Wrapper>
-      <div className="py-8 z-10">
-        <div className="text-center pb-10 pt-2 mx-auto  md:max-w-5xl">
-          <SectionSubHeading>Section Title</SectionSubHeading>
-          <div className="text-left">
-            <Content>
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum
-              ratione pariatur dolorum fugit harum, qui soluta ullam,
-              perferendis vero autem, possimus reprehenderit dolor? Illum atque
-              sint eaque, non eos magnam?
-            </Content>
-          </div>
-        </div>
-
-        <div className="grid gap-0 xl:gap-20 grid-cols-1 xl:grid-cols-[1fr_3fr]">
-          <div className="mt-4 order-2 xl:order-1">
-            <SectionHeading>{title}</SectionHeading>
-            <div className="mt-4">
-              <Link href={button.link}>
-                <Button color="default" fullWidth>
-                  {button.label}
-                </Button>
-              </Link>
-            </div>
-          </div>
-
-          <div className="leading-8 content-container order-1">
-            {description}
-          </div>
+  <>
+    <section>
+      <Hero img={bannerImg} height={500} isVideo priority />
+    </section>
+    <section>
+      <div className="text-white text-center my-8 py-16 px-8 mx-auto md:max-w-5xl bg-grey-600">
+        <h3 className="font-black text-xl mb-8 uppercase">Section Title</h3>
+        <div className="text-left">
+          <Content textWhite>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illum
+            ratione pariatur dolorum fugit harum, qui soluta ullam, perferendis
+            vero autem, possimus reprehenderit dolor? Illum atque sint eaque,
+            non eos magnam?
+          </Content>
         </div>
       </div>
-    </Wrapper>
-  </section>
+    </section>
+    <section>
+      <Wrapper>
+        <div className="pb-8 z-10">
+          <div className="grid gap-0 xl:gap-20 grid-cols-1 xl:grid-cols-[1fr_3fr]">
+            <div className="mt-4 order-2 xl:order-1">
+              <SectionHeading>{title}</SectionHeading>
+              <div className="mt-4">
+                <Link href={button.link}>
+                  <Button color="default" fullWidth>
+                    {button.label}
+                  </Button>
+                </Link>
+              </div>
+            </div>
+
+            <div className="leading-8 content-container order-1">
+              {description}
+            </div>
+          </div>
+        </div>
+      </Wrapper>
+    </section>
+  </>
 )
 
 export default HomeHeroSection
