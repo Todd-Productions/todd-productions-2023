6e92f4fa5a2e0f795619585e302fc08d34c15745
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2024-01-08 14:01:34 -0800

    Updated HomeHeroSection

--- src/ui/atoms/Content/Content.tsx
+++ src/ui/atoms/Content/Content.tsx
@@ -1,13 +1,21 @@
 import React, { ReactNode } from "react"
+import cns from "classnames"
 
 import "./content.css"
 
 interface ContentProps {
   children: ReactNode
+  textWhite?: boolean
 }
 
-const Content: React.FC<ContentProps> = ({ children }) => (
-  <div className="content leading-8 text-grey-600">{children}</div>
+const Content: React.FC<ContentProps> = ({ children, textWhite }) => (
+  <div
+    className={cns("content leading-8 text-grey-600", {
+      "text-white": textWhite,
+    })}
+  >
+    {children}
+  </div>
 )
 
 export default Content
--- src/ui/molecules/HomeHeroSection/HomeHeroSection.tsx
+++ src/ui/molecules/HomeHeroSection/HomeHeroSection.tsx
@@ -1,7 +1,14 @@
 import React, { ReactNode } from "react"
 import Link from "next/link"
 
-import { Button, SectionHeading, Wrapper, Hero } from "../../atoms"
+import {
+  Button,
+  SectionHeading,
+  Wrapper,
+  Hero,
+  Content,
+  SectionSubHeading,
+} from "../../atoms"
 import { IButton } from "../../../types"
 
 export interface IPageInfo {
@@ -19,29 +26,58 @@ const HomeHeroSection: React.FC<SplitContentProps> = ({
   button,
   bannerImg,
 }) => (
-  <section>
-    <Hero img={bannerImg} height={500} isVideo priority />
-    <Wrapper>
-      <div className="py-8 z-10">
-        <div className="grid gap-0 xl:gap-20 grid-cols-1 xl:grid-cols-[1fr_3fr]">
-          <div className="mt-4 order-2 xl:order-1">
-            <SectionHeading>{title}</SectionHeading>
-            <div className="mt-4">
-              <Link href={button.link}>
-                <Button color="default" fullWidth>
-                  {button.label}
-                </Button>
-              </Link>
+  <>
+    <section>
+      <Hero img={bannerImg} height={500} isVideo priority />
+    </section>
+    <section className="bg-grey-600">
+      <div className="text-white text-center mb-8 py-16 px-8 mx-auto md:max-w-6xl">
+        <h3 className="font-black text-4xl mb-8 uppercase">
+          Marking our 25th year as Todd Productions
+        </h3>
+        <div className="text-left mb-8">
+          <Content textWhite>
+            We are thrilled to announce that Todd Productions Inc. is
+            celebrating its 25th year in business on January 15. Under the name
+            that was conceived at the tender age of ten, Todd Stanton and his
+            team have been producing high-quality content that always aims to
+            entertain, educate and inspire our audiences. We are grateful for
+            the support of our partners, clients and fans who have made this
+            journey possible. You are the reason we do what we do! As our
+            founder and CEO Todd Stanton says, &quot;We believe that every story
+            deserves to be told in a captivating and engaging way – digitally
+            and masterfully.&quote;
+          </Content>
+        </div>
+        <SectionSubHeading isWhite>
+          Now the question begs itself, what will Todd Productions, the Sequel,
+          have in store for you?
+        </SectionSubHeading>
+      </div>
+    </section>
+    <section>
+      <Wrapper>
+        <div className="py-8 z-10">
+          <div className="grid gap-0 xl:gap-20 grid-cols-1 xl:grid-cols-[1fr_3fr]">
+            <div className="mt-4 order-2 xl:order-1">
+              <SectionHeading>{title}</SectionHeading>
+              <div className="mt-4">
+                <Link href={button.link}>
+                  <Button color="default" fullWidth>
+                    {button.label}
+                  </Button>
+                </Link>
+              </div>
+            </div>
+
+            <div className="leading-8 content-container order-1">
+              {description}
             </div>
           </div>
-
-          <div className="leading-8 content-container order-1">
-            {description}
-          </div>
         </div>
-      </div>
-    </Wrapper>
-  </section>
+      </Wrapper>
+    </section>
+  </>
 )
 
 export default HomeHeroSection
