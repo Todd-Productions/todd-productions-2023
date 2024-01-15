a7ec395ceac07370039641338ee5598366758407
Andrew Stanton <andrew@toddprod.com>
2024-01-15 15:15:38 -0500

    Updating content

--- src/ui/molecules/HomeHeroSection/HomeHeroSection.tsx
+++ src/ui/molecules/HomeHeroSection/HomeHeroSection.tsx
@@ -3,6 +3,9 @@ import React, { ReactNode } from "react"
 
 import { IButton, ISocialLinks } from "../../../types"
 import { Button, Content, Hero, SectionHeading, Wrapper } from "../../atoms"
+import Social from "../Social/Social"
+
+import "./styles.css"
 
 export interface IPageInfo {
   title: string
@@ -26,7 +29,7 @@ const HomeHeroSection: React.FC<HomeHeroSectionProps> = (props) => {
           <h3 className="font-black text-4xl mb-8 uppercase">
             Todd Productions: the Sequel
           </h3>
-          <div className="text-left mb-8">
+          <div className="text-left mb-2s">
             <Content textWhite>
               <p>
                 We are thrilled to announce that Todd Productions Inc. is
@@ -35,14 +38,35 @@ const HomeHeroSection: React.FC<HomeHeroSectionProps> = (props) => {
                 have been producing high-quality content that always aims to
                 entertain, educate and inspire our audiences. We are grateful
                 for the support of our partners, clients and fans who have made
-                this journey possible. You are the reason we do what we do! As
-                our founder and CEO Todd Stanton says,{" "}
+                this journey possible. You are the reason we do what we do!
+              </p>
+              <p>
+                As our founder and CEO Todd Stanton says, &quot;We believe that
+                every story deserves to be told in a captivating and engaging
+                way – digitally and masterfully.&quot;
+              </p>
+              <p>
                 <em>
-                  &quot;We believe that every story deserves to be told in a
-                  captivating and engaging way – digitally and
-                  masterfully.&quot;
+                  What&apos;s next for Todd Productions? To celebrate its 25th
+                  business anniversary, the company will debut a new
+                  documentary, titled &quot;Todd Productions: 25 Years of
+                  Creative Media&quot; which will stream on{" "}
+                  <a href={facebook} target="_blank" rel="noreferrer">
+                    Facebook
+                  </a>{" "}
+                  and{" "}
+                  <a href={youtube} target="_blank" rel="noreferrer">
+                    YouTube
+                  </a>{" "}
+                  on <strong>January 23</strong>.
                 </em>
               </p>
+              <div
+                className="justify-center mx-auto content-center items-center mt-10 text-center"
+                style={{ width: "120px" }}
+              >
+                <Social facebook={facebook} youtube={youtube} fill />
+              </div>
             </Content>
           </div>
         </div>
