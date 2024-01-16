4eb1222c2bda58df2c0babe6f68fe0a2c136d4f7
eduardo.verdusco@yahoo.com <eduardo.verdusco@yahoo.com>
2024-01-16 10:26:10 -0800

    Updated content, added new transparent logo

--- src/ui/molecules/HomeHeroSection/HomeHeroSection.tsx
+++ src/ui/molecules/HomeHeroSection/HomeHeroSection.tsx
@@ -1,5 +1,6 @@
-import Link from "next/link"
 import React, { ReactNode } from "react"
+import Link from "next/link"
+import Image from "next/image"
 
 import { IButton, ISocialLinks } from "../../../types"
 import { Button, Content, Hero, SectionHeading, Wrapper } from "../../atoms"
@@ -24,50 +25,67 @@ const HomeHeroSection: React.FC<HomeHeroSectionProps> = (props) => {
         <Hero img={bannerImg} height={500} isVideo priority />
       </section>
       <section className="bg-grey-600 announcement">
-        <div className="text-white text-center mb-8 py-16 px-8 mx-auto md:max-w-6xl">
-          <h3 className="font-black text-4xl mb-8 uppercase">
-            Todd Productions: the Sequel
-          </h3>
-          <div className="text-left mb-2s">
-            <Content textWhite>
-              <p>
-                We are thrilled to announce that Todd Productions Inc. is
-                celebrating its 25th year in business. Under the name that was
-                conceived at the tender age of ten, Todd Stanton and his team
-                have been producing high-quality content that always aims to
-                entertain, educate and inspire our audiences. We are grateful
-                for the support of our partners, clients and fans who have made
-                this journey possible. You are the reason we do what we do!
-              </p>
-              <p>
-                As our founder and CEO Todd Stanton says, &quot;We believe that
-                every story deserves to be told in a captivating and engaging
-                way – digitally and masterfully.&quot;
-              </p>
-              <p>
-                <em>
-                  What&apos;s next for Todd Productions? To celebrate its 25th
-                  business anniversary, the company will debut a new
-                  documentary, titled &quot;Todd Productions: 25 Years of
-                  Creative Media&quot; which will stream on{" "}
-                  <a href={facebook} target="_blank" rel="noreferrer">
-                    Facebook
-                  </a>{" "}
-                  and{" "}
-                  <a href={youtube} target="_blank" rel="noreferrer">
-                    YouTube
-                  </a>{" "}
-                  on <strong>January 23</strong>.
-                </em>
-              </p>
-              <div className="justify-center mx-auto content-center items-center mt-10 text-center">
-                <Link href="/25-year-announcement">
-                  <Button>View More Details</Button>
-                </Link>{" "}
-              </div>
-            </Content>
+        <Wrapper>
+          <div className="text-white grid gap-y-10 md:gap-y-0 md:grid-cols-[1fr_2fr] py-10 gap-x-10">
+            <div className="flex flex-col items-center md:items-start">
+              <h3 className="font-black text-4xl mb-8 uppercase">
+                Todd Productions: the Sequel
+              </h3>
+              <Image
+                src="/images/logo-25-years.png"
+                alt="logo"
+                width={250}
+                height={300}
+                priority
+              />
+            </div>
+            <div className="text-right mb-2s">
+              <Content textWhite>
+                <p>
+                  We are thrilled to announce that Todd Productions Inc. is
+                  celebrating its 25th year in business. Under the name that was
+                  conceived at the tender age of ten, Todd Stanton and his team
+                  have been producing high-quality content that always aims to
+                  entertain, educate and inspire our audiences. We are grateful
+                  for the support of our partners, clients and fans who have
+                  made this journey possible. You are the reason we do what we
+                  do!
+                </p>
+                <p>
+                  As our founder and CEO Todd Stanton says, &quot;We believe
+                  that every story deserves to be told in a captivating and
+                  engaging way – digitally and masterfully.&quot;
+                </p>
+                <p>
+                  <em>
+                    What&apos;s next for Todd Productions? To celebrate its 25th
+                    business anniversary, the company will debut a new
+                    documentary, titled &quot;Todd Productions: 25 Years of
+                    Creative Media&quot; which will stream on{" "}
+                    <a href={facebook} target="_blank" rel="noreferrer">
+                      Facebook
+                    </a>{" "}
+                    and{" "}
+                    <a href={youtube} target="_blank" rel="noreferrer">
+                      YouTube
+                    </a>{" "}
+                    on <strong>January 23</strong>.
+                  </em>
+                </p>
+                <p>
+                  The company will debut a new documentary, titled &quot;Todd
+                  Productions: 25 Years of Creative Media&quot; which on January
+                  23 at 10a.m. Watch below or visit Facebook and Youtube
+                </p>
+                <div className="justify-center mx-auto content-center items-center mt-10 text-center">
+                  <Link href="/25-year-announcement">
+                    <Button>get the details </Button>
+                  </Link>{" "}
+                </div>
+              </Content>
+            </div>
           </div>
-        </div>
+        </Wrapper>
       </section>
       <section>
         <Wrapper>
